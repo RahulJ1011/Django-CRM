163e9198724fc87cb77b68431472dcce542b777d
rahulj1011 <rahuljr2022@gmail.com>
2025-01-16 19:40:12 +0530

    configured

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -1,6 +1,7 @@
 
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,7 +45,7 @@ ROOT_URLCONF = "dcrm.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+       "DIRS": [BASE_DIR / "website" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
