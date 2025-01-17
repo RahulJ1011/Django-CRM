8ca0a606f6b57ee3aabe73b79a9d257a784e2906
rahulj1011 <rahuljr2022@gmail.com>
2025-01-17 16:44:11 +0530

    added logout

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 
@@ -7,14 +7,23 @@ def home(request):
         username = request.POST['UserName']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,"You have been logged in!")
+            return redirect('home')
+        else:
+            messages.error(request,"Invalid username or password")
     return render(request, 'home.html',{})
 
 def login_user(request):
+    
     pass
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request,"You have been logged out ........")
+    return redirect('home')
 
 
 
