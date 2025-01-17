6a01be84df193615cfb2bea448b2ef5c4472c1d1
rahulj1011 <rahuljr2022@gmail.com>
2025-01-17 16:21:34 +0530

    added login

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-   path('',views.home,name='home')
+   path('',views.home,name='home'),
+   #path('', views.login_user,name='login'),
+   #path('',views.logout_user,name="logout")
 ]
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 def home(request):
+    if request.method == 'POST':
+        username = request.POST['UserName']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
     return render(request, 'home.html',{})
+
+def login_user(request):
+    pass
+
+
+def logout_user(request):
+    pass
+
+
 
