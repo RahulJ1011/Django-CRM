033ef3be06bb96cbce8b09961b4b86f2c4e18590
rahulj1011 <rahuljr2022@gmail.com>
2025-01-21 10:11:44 +0530

    Register page completed

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
    path('',views.home,name='home'),
-   path('', views.login_user,name='login')
-   #path('',views.logout_user,name="logout")
+   #path('', views.login_user,name='login')
+   path('logout/',views.logout_user,name="logout"),
+   path('register/',views.register_user,name='register')
 ]
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-
+from .forms import SignupForm
 def home(request):
     if request.method == 'POST':
-        username = request.POST['UserName']
-        password = request.POST['password']
+        username = request.POST.get('UserName', None)
+        password = request.POST.get('password', None)
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
@@ -24,6 +24,26 @@ def logout_user(request):
     logout(request)
     messages.success(request,"You have been logged out ........")
     return redirect('home')
+
+
+def register_user(request):
+
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request,"You have sucessfully registered!!!!!!")
+            return redirect('home')
+        
+    else:
+        form = SignupForm()
+        return render(request,'register.html',{'form':form})
+    return render(request,'register.html',{'form':form})
 
 
 
